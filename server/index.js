3659926e981e7dfa77362705621014c15f4f7c8f
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-06 20:05:16 -0700

    add /friends and /friends/:id routes to index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function(req, res) {
+//this route for server side validation with Google
+//see https://developers.google.com/identity/sign-in/android/backend-auth
+app.post('/api/validate', function(req, res) {
   res.send('hello world');
 });
+
+app.route('/friends')
+  .get(function (req, res) {
+    res.send('Get all friends');
+  })
+  .post(function (req, res) {
+    res.send('Add a friend');
+  });
+
+app.route('/friends/:id')
+  .get(function (req, res) {
+    res.send('Get one friend');
+  })
+  .put(function (req, res) {
+    res.send('Update a friends name or number');
+  })
 
 let port = process.env.PORT || 1337;
 
