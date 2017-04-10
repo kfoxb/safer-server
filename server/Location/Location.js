7c2a8e63b1e23bc9d77b31cb518a447d95788586
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-09 21:22:23 -0700

    change all routes to respond with JSON and a status of 200

--- server/Location/Location.js
+++ server/Location/Location.js
@@ -1,5 +1,9 @@
 const Users = require('../../db/Users/Users.js');
 const Location = require('../../db/Location/Location.js');
 
-exports.updateCoordinates = () => {};
-exports.updatePrivacy = () => {};
+exports.updateCoordinates = () => {
+  res.status(200).json({});
+};
+exports.updatePrivacy = () => {
+  res.status(200).json({});
+};
--- server/Users/Users.js
+++ server/Users/Users.js
@@ -4,17 +4,17 @@ const Labels = require('../../db/Labels/Labels.js');
 const Location = require('../../db/Location/Location.js');
 
 exports.addFriend = (req, res) => {
-  
+  res.status(200).json({});
 };
 
 exports.getAllFriends = (req, res) => {
-  
+  res.status(200).json({});
 };
 
 exports.getFriendById = (req, res) => {
-  
+  res.status(200).json({});
 };
 
 exports.updateFriendById = (req, res) => {
-  
+  res.status(200).json({});
 };
