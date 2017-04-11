39520330647a69ae380a36e47e9f1230f7f7ff76
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-10 20:26:35 -0700

    remove reference to personalPrivacy in Authorization.js

--- server/Authorization/Authorization.js
+++ server/Authorization/Authorization.js
@@ -1,5 +1,4 @@
 const Users = require('../../db/Users/Users.js');
-const PersonalPrivacy = require('../../db/PersonalPrivacy/PersonalPrivacy.js');
 
 module.exports = (req, res, next) => {
   console.log('hi');
