b740d3b5aa46a12c1852ac33f7be2879fe8aa67f
Dario Arteaga <daartv@gmail.com>
2017-04-13 11:42:18 -0700

    Storing fences on the database correctly

--- db/Labels/Labels.js
+++ db/Labels/Labels.js
@@ -20,17 +20,3 @@ let Labels = db.define('Labels', {
 Labels.sync();
 
 module.exports = Labels;
-
-//label: Sequelize.STRING,
-//lat_tlc: Sequelize.STRING,
-//lng_tlc: Sequelize.STRING,
-//lat_trc: Sequelize.STRING,
-//lng_trc: Sequelize.STRING,
-//lat_brc: Sequelize.STRING,
-//lng_brc: Sequelize.STRING,
-//lat_lbc: Sequelize.STRING,
-//lng_lbc: Sequelize.STRING,
-//lat_init: Sequelize.STRING,
-//lng_init: Sequelize.STRING
-
-//Label.belongsTo(User);
--- server/Labels/Labels.js
+++ server/Labels/Labels.js
@@ -1,6 +1,10 @@
 const Users = require('../../db/Users/Users.js');
 const Contacts = require('../../db/Contacts/Contacts.js');
 const Labels = require('../../db/Labels/Labels.js');
+<<<<<<< HEAD
+=======
+const Location = require('../../db/Location/Location.js');
+>>>>>>> Storing fences on the database correctly
 const phone = require('phone');
 
 exports.addLabel = (req, res) => {
@@ -10,6 +14,7 @@ exports.addLabel = (req, res) => {
     }
   })
   .then((user) => {
+<<<<<<< HEAD
     console.log('user when creating a Label/fence', user);
     Labels.create({
       userId: user.dataValues.id,
@@ -58,3 +63,29 @@ exports.getAllFences = (req, res) => {
 
 //Need to get the user's phone number from the params of the url
 //there's no body on a get request
+=======
+	Labels.create({
+    UserId: user.dataValues.id,
+    label: req.body.label,
+    lat_tlc: req.body.polygon[0].lat,
+    lng_tlc: req.body.polygon[0].lng,
+    lat_trc: req.body.polygon[1].lat,
+    lng_trc: req.body.polygon[1].lng,
+    lat_brc: req.body.polygon[2].lat,
+    lng_brc: req.body.polygon[2].lng,
+    lat_lbc: req.body.polygon[3].lat,
+    lng_lbc: req.body.polygon[3].lng,
+    lat_init: req.body.polygon[4].lat,
+    lng_init: req.body.polygon[4].lat
+  })
+    .then((fence) => {
+      res.status(201).json({});
+      console.log('everything ok')
+    })
+    .catch((error) => {
+      console.log('errored out', error)
+      res.status(418).json(`I'm a teapot`);
+    })
+  })
+}
+>>>>>>> Storing fences on the database correctly
