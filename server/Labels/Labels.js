ed2750d79cdc49fe1e3415d5d6e050abb9ffdd11
Dario Arteaga <daartv@gmail.com>
2017-04-13 19:36:39 -0700

    Removed a obsolete requirement and a file that got committed by mistake

--- server/Labels/Labels.js
+++ server/Labels/Labels.js
@@ -1,7 +1,6 @@
 const Users = require('../../db/Users/Users.js');
 const Contacts = require('../../db/Contacts/Contacts.js');
 const Labels = require('../../db/Labels/Labels.js');
-const Location = require('../../db/Location/Location.js');
 const phone = require('phone');
 
 exports.addLabel = (req, res) => {
