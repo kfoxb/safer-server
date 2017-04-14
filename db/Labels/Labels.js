f1b7d675e15fda944122d143cab47c6d4312e51a
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-14 15:11:03 -0700

    change table names for labels and fix some syntax errors

--- db/Labels/Labels.js
+++ db/Labels/Labels.js
@@ -5,16 +5,16 @@ const User = require('../Users/Users.js');
 let Labels = db.define('Labels', {
   userId: Sequelize.INTEGER,
   label: Sequelize.STRING,
-  lat_tlc: Sequelize.STRING,
-  lng_tlc: Sequelize.STRING,
-  lat_trc: Sequelize.STRING,
-  lng_trc: Sequelize.STRING,
-  lat_brc: Sequelize.STRING,
-  lng_brc: Sequelize.STRING,
-  lat_lbc: Sequelize.STRING,
-  lng_lbc: Sequelize.STRING,
-  lat_init: Sequelize.STRING,
-  lng_init: Sequelize.STRING
+  latTlc: Sequelize.STRING,
+  lngTlc: Sequelize.STRING,
+  latTrc: Sequelize.STRING,
+  lngTrc: Sequelize.STRING,
+  latBrc: Sequelize.STRING,
+  lngBrc: Sequelize.STRING,
+  latLbc: Sequelize.STRING,
+  lngLbc: Sequelize.STRING,
+  latInit: Sequelize.STRING,
+  lngInit: Sequelize.STRING
 });
 
 Labels.sync();
