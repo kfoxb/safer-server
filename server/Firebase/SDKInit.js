5c3b3d5d76d21c3250915fb2ce9d3c74248a219b
Tiffany <tiffanylin27@gmail.com>
2017-04-14 20:02:39 -0700

    Add todos and references for future use

--- server/Firebase/SDKInit.js
+++ server/Firebase/SDKInit.js
@@ -6,3 +6,10 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-safer.firebaseio.com"
 });
+
+
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+//TODO: Set up messaging for an individual device
+// https://firebase.google.com/docs/cloud-messaging/admin/send-messages
+//TODO: Set up messaging for multiple devices
+// https://firebase.google.com/docs/cloud-messaging/android/send-multiple
