3f28c2e0d598847fe8db2a3df5595cb049e570fb
Tiffany <tiffanylin27@gmail.com>
2017-04-15 11:27:47 -0700

    Implement server side notifications

--- server/Firebase/Firebase.js
+++ server/Firebase/Firebase.js
@@ -3,11 +3,32 @@ const Users = require('../../db/Users/Users.js');
 const serverKey = require('../Firebase/fcm-server-key.js').serverKey;
 const axios = require('axios');
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-safer.firebaseio.com"
+});
+
 exports.updateFCMToken = (req, res) => {
   //TODO: move the notification sending logic elsewhere
   //currently sending a notification upon loading the app
-  axios.get('https://fcm.googleapis.com/fcm/send')
+  var payload = {
+    notification: {
+      title: 'From the server',
+      text: 'Yooo to team blink672'
+    }
+  }
 
+  admin.messaging().sendToDevice(req.body.FCMToken, payload)
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
   //TODO: set a userId on the req.body to query the db with
   //currently hardcoding to 1
