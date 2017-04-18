accf5349ac8306a13f8b9840699c501f95ecf4a9
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-18 15:27:45 -0700

    implement mock authentication
    this does not actually provide any security and will just provide us
    with a way to test things with actual Google accounts. The Google
    accounts are secure, but the data in our api is accessible by anyone who
    can provide any Google account information, that's why it's insecure.

--- server/Authorization/Authorization.js
+++ server/Authorization/Authorization.js
@@ -1,6 +1,32 @@
 const Users = require('../../db/Users/Users.js');
 
 module.exports = (req, res, next) => {
-  req.user = {id: 1};
-  next();
+  if (req.headers.authorization === undefined) {
+    let errMsg = 'Error: No Authorization header in request';
+    console.log(errMsg);
+    res.status(401).json(errMsg);
+  } else {
+    let userProfile = JSON.parse(req.headers.authorization);
+    let firstName = userProfile.name.slice(0, userProfile.name.indexOf(' '));
+    let lastName = userProfile.name.slice(userProfile.name.indexOf(' ') + 1);
+    let email = userProfile.email;
+    Users.findOrCreate({where: {email: email},
+      defaults: {
+        first: firstName,
+        phoneNumber: `+1${userProfile.phoneNumber}`,
+        last: lastName,
+        email: email,
+        lat: '',
+        long: ''
+      }})
+    .spread((createdUser, wasCreated) => {
+      req.user = createdUser.get();
+      next(req, res);
+    })
+    .catch((err) => {
+      let errMsg = 'Error: OAuth Token Invalid';
+      console.log(errMsg, err);
+      res.status(401).json(errMsg);
+    });
+  }
 };
