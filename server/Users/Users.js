3c5f7234fd0bd4e50da42a518fc74defac6b93ca
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-19 14:26:56 -0700

    implement /api/user route to update a users information

--- server/Users/Users.js
+++ server/Users/Users.js
@@ -32,6 +32,7 @@ exports.addFriend = (req, res) => {
   });
 };
 
+
 // TODO: Second half
 //http://www.datchley.name/promise-patterns-anti-patterns/
 exports.getAllFriendData = (req, res, next) => {
@@ -170,6 +171,17 @@ exports.updatePrivacy = (req, res) => {
   })
   .catch((err) => {
     console.error('There was an error updating the user privacy info: ', err);
+    res.status(500).json({err: err});
+  });
+};
+
+exports.updateUser = (req, res) => {
+  Users.update(req.body, {where: {id: req.user.id}})
+  .then(() => {
+    res.status(200).send();
+  })
+  .catch((err) => {
+    console.error('There was an error updating the user: ', err);
     res.status(500).json({err: err});
   });
 };
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const {
   updateCoordinates,
   getContactInformation,
   findUserWithPhoneNumber,
+  updateUser
 } = require('./Users/Users.js');
 
 const { updateFCMToken } = require('./Firebase/Firebase.js');
@@ -20,12 +21,15 @@ const { addGroup, getGroups, getGroupUsers } = require('./Groups/Groups.js');
 const authorization = require('./Authorization/Authorization.js');
 const {addLabel, getAllFences} = require('./Labels/Labels.js');
 
-app.use(authorization);
-app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
+app.use(authorization);
 
 app.route('/api/users/location')
   .put(updateCoordinates);
+
+app.route('/api/user/')
+  .put(updateUser);
 
 app.route('/api/labels')
   .get(getAllFences)
