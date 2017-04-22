89dac23f155a8587250ad1e3f0be2bef0f35b64e
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-21 19:24:44 -0700

    delete old comment

--- db/config.js
+++ db/config.js
@@ -5,8 +5,4 @@ const db = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
   logging: true
 });
 
-// db.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`).spread(function(results, metadata) {
-//   console.log(`creating DATABASE ${dbName}, if it does not exist`);
-// });
-
 module.exports = db;
