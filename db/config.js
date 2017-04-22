7d474a296d912ef0b88b08b5d85c63b22c4c8c94
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-21 18:51:03 -0700

    remove db creation and enable logging

--- db/config.js
+++ db/config.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize');
 const dbName = 'saferDb';
 
 const db = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
-  logging: false
+  logging: true
 });
 
-db.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`).spread(function(results, metadata) {
-  console.log(`creating DATABASE ${dbName}, if it does not exist`);
-});
+// db.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`).spread(function(results, metadata) {
+//   console.log(`creating DATABASE ${dbName}, if it does not exist`);
+// });
 
 module.exports = db;
