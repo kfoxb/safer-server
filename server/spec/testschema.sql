e8d57d254f130713d58adde456e0fc9b6504ba6b
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-25 15:16:07 -0700

    add test schema and change npm test script to use it

---
+++ server/spec/testschema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS saferDbTesting;
+
+CREATE DATABASE saferDbTesting;
+
+USE saferDbTesting;
+
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < testschema.sql
+ *  to create the database and the tables.*/
